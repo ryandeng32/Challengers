6e767066ce4348993716807b6138b456b4aa9d6f
Ryan Deng <ryan.deng32@gmail.com>
2020-08-12 15:40:01 -0400

    Finish user authentication backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
-// app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/auth", require("./routes/api/auth"));
 // app.use("/api/profile", require("./routes/api/profile"));
 // app.use("/api/posts", require("./routes/api/posts"));
 
