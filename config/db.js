e0271f4c006e8393a4b0cef1e47ace15dcc3b829
Ryan Deng <ryan.deng32@gmail.com>
2020-08-17 17:54:30 -0400

    prepare for deploy and add production.json

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || db, {
+    await mongoose.connect(process.env.MONGO_URI || config.get("mongoURI"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
