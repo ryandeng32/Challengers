98ea07389e7d39191ef785a7483e84e11ba43464
Ryan Deng <ryan.deng32@gmail.com>
2020-08-17 22:39:13 -0400

    change group to groups

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 // Mongoose Model
 const Group = require("../../models/Group");
 const User = require("../../models/User");
 
-// @route       GET api/group/me
+// @route       GET api/groups/me
 // @desc        Get current user's joined groups
 // @access      Private
 router.get("/me", auth, async (req, res) => {
@@ -25,4 +26,18 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route       POST api/group/
+// @desc        Create or update group
+// @access      Private
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 module.exports = router;
