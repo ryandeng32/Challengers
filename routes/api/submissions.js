5bfb485213126e11264c0cffbabf6011fe5792fd
Ryan Deng <ryan.deng32@gmail.com>
2020-08-19 22:37:19 -0400

    finish backend for submissions

--- routes/api/submissions.js
+++ routes/api/submissions.js
@@ -101,4 +101,109 @@ router.delete("/:sub_id", [auth, checkObjectId("sub_id")], async (req, res) => {
   }
 });
 
+// @route       PUT /submissions/like/:sub_id
+// @desc        Like a submission
+// @access      Private
+router.put(
+  "/like/:sub_id",
+  [auth, checkObjectId("sub_id")],
+  async (req, res) => {
+    try {
+      const submission = await Submission.findById(req.params.sub_id);
+      if (!submission) {
+        return res
+          .status(400)
+          .json({ msg: "There is no submission with the given id" });
+      }
+      // Unlike if it's liked by current user already
+      if (submission.likes.some((el) => el.user.toString() === req.user.id)) {
+        submission.likes = submission.likes.filter(
+          (like) => like.user.toString() !== req.user.id
+        );
+      } else {
+        submission.likes.unshift({ user: req.user.id });
+      }
+      await submission.save();
+      res.json(submission.likes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route       POST submissions/comment/:sub_id
+// @desc        Comment on a submission
+// @access      Private
+router.post(
+  "/comment/:sub_id",
+  [
+    auth,
+    checkObjectId("sub_id"),
+    check("text", "Text is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const submission = await Submission.findById(req.params.sub_id);
+      if (!submission) {
+        return res
+          .status(400)
+          .json({ msg: "There is no submission with the given id" });
+      }
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+      };
+      submission.comments.unshift(newComment);
+      await submission.save();
+      res.json(submission.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route       DELETE submissions/:sub_id/comment/:comment_id
+// @desc        Delete a comment
+// @access      Private
+router.delete(
+  "/:sub_id/comment/:comment_id",
+  [auth, checkObjectId("sub_id"), checkObjectId("comment_id")],
+  async (req, res) => {
+    try {
+      const submission = await Submission.findById(req.params.sub_id);
+      if (!submission) {
+        return res
+          .status(400)
+          .json({ msg: "There is no submission with the given id" });
+      }
+      const comment = submission.comments.filter(
+        (item) => item._id.toString() === req.params.comment_id
+      )[0];
+      if (comment.length === 0) {
+        return res.status(400).json({ msg: "Comment does not exist!" });
+      }
+      // Check if user created the comment
+      if (comment.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: "User not authorized" });
+      }
+      submission.comments = submission.comments.filter(
+        (item) => item._id !== comment._id
+      );
+      await submission.save();
+      res.json(submission.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 module.exports = router;
